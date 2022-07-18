52eded3a240b0623cc24827e9ca9340cece8e1fc
CS <you@example.com>
2022-07-18 19:24:43 +0800

    pro

--- fon/basics.py
+++ fon/basics.py
@@ -1,8 +1,23 @@
+from numbers import Number
+
 from typing import Any, List
+
+
+def all_bool(xs) -> bool:
+    return all(map(is_bool, xs))
+
+def all_int(xs) -> bool:
+    return all(map(is_int, xs))
+
+def all_str(xs) -> bool:
+    return all(map(is_str, xs))
+
+
 
 
 def is_bool(x) -> bool:
     return isinstance(x, bool)
+
 
 def is_float(x) -> bool:
     return isinstance(x, float)
@@ -15,6 +30,9 @@ def is_iterable(x) -> bool:
 
 def is_list(x) -> bool:
     return isinstance(x, list)
+
+def is_number(x) -> bool:
+    return isinstance(x, Number)
 
 def is_str(x) -> bool:
     return isinstance(x, str)
